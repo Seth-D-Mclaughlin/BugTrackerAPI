dbc244b7c4b2faba9da517740ff4c41b6cff2988
Seth-D-McLaughlin <seth.d.mclaughlin@gmail.com>
2021-07-26 17:14:02 -0400

    Setup DbContext class

--- BugTracker.WebAPI/Startup.cs
+++ BugTracker.WebAPI/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BugTracker.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,6 +18,7 @@ namespace BugTracker.WebAPI
 {
     public class Startup
     {
+        private string _connection = null;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,7 +29,11 @@ namespace BugTracker.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            var builder = new Microsoft.Data.SqlClient.SqlConnectionStringBuilder(
+                Configuration.GetConnectionString("DefaultConnection"));
+                builder.Password = Configuration["DbPassword"];
+                _connection = builder.ConnectionString;
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(_connection));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
