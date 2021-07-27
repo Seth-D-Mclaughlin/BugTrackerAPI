57c3ce7a49cc94100a8706adce51a3e5be24db30
Seth-D-McLaughlin <seth.d.mclaughlin@gmail.com>
2021-07-27 12:17:45 -0400

    Added Migrations

--- BugTracker.Data/ApplicationDbContext.cs
+++ BugTracker.Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using BugTracker.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.Data
@@ -5,5 +6,6 @@ namespace BugTracker.Data
     public class ApplicationDbContext: DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+        public DbSet<UserEntity> Users {get; set;}
     }
 }
